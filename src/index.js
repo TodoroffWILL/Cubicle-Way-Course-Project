e7a9979d34d1844987c370bc2bbcc2cdd0a5c5f7
Doncho Todorov <108018258+TodoroffWILL@users.noreply.github.com>
2023-05-28 23:39:26 +0300

    setting up express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+const express = require('express');
 
+const app = express();
+const PORT = 5000;
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello from express');
+});
+
+app.listen(PORT, () => {
+  console.log('Server is running on port 5000');
+});
