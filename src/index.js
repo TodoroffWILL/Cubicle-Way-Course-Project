dfa38827c8454939422d6ec8fad206deefe0f30b
Doncho Todorov <108018258+TodoroffWILL@users.noreply.github.com>
2023-05-28 23:47:26 +0300

    add handlebars

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const handlebars = require('express-handlebars');
 
 const app = express();
 const PORT = 5000;
 
+//Handlebars config
+app.engine(
+  'hbs',
+  handlebars.engine({
+    extname: 'hbs',
+  })
+);
+app.set('view engine', 'hbs');
+app.set('views', 'src/views'); // Where to look for the folder cause default is "root" now chaning to view
+// Routes
+
 app.get('/', (req, res) => {
-  res.status(200).send('Hello from express');
+  res.status(200).render('index');
 });
 
 app.listen(PORT, () => {
