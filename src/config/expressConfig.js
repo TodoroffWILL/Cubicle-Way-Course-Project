6c2724872191aca563ae6db95201abb3b37b7d1f
Doncho Todorov <108018258+TodoroffWILL@users.noreply.github.com>
2023-06-09 11:49:20 +0300

    back on track

--- src/config/expressConfig.js
+++ src/config/expressConfig.js
@@ -5,7 +5,6 @@ function expressConfig(app) {
   // We have to take the express so we can use static
   app.use(express.static(path.resolve(__dirname, '../public')));
   app.use(express.urlencoded({ extended: false }));
-  
 }
 
 module.exports = expressConfig;
