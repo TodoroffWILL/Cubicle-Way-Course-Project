62be37107021aa376272dd5ea2dc4feff916d262
Doncho Todorov <108018258+TodoroffWILL@users.noreply.github.com>
2023-06-10 17:50:44 +0300

    hashing password before saving to DB

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+//bcrypting/hashing the password
+const bcrypt = require('bcrypt');
 
 const userSchema = mongoose.Schema({
   username: String,
@@ -11,6 +13,12 @@ userSchema.virtual('repeatPassword').set(function (value) {
   if (value !== this.password) {
     throw new mongoose.MongooseError('Password missmatch!');
   }
+});
+
+// Направи хеша преди да сейвнеш в базата данни
+userSchema.pre('save', async function () {
+  const hash = await bcrypt.hash(this.password, 10);
+  this.password = hash;
 });
 
 const User = mongoose.model('User', userSchema);
