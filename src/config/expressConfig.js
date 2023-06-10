3d43d59978edf4d2747a764b3aff2b1de7fee12d
Doncho Todorov <108018258+TodoroffWILL@users.noreply.github.com>
2023-06-10 18:32:46 +0300

    login page added

--- src/config/expressConfig.js
+++ src/config/expressConfig.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
+const cookieParser = require('cookie-parser')
 
 function expressConfig(app) {
   // We have to take the express so we can use static
   app.use(express.static(path.resolve(__dirname, '../public')));
   app.use(express.urlencoded({ extended: false }));
+  app.use(cookieParser());
 }
 
 module.exports = expressConfig;
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,15 +2,31 @@ const router = require('express').Router();
 
 const userManager = require('../managers/userManager');
 
-router.get('/register', (req, res) => {
-  res.render('user/registerPage');
-});
+router
+  .route('/register')
+  .get((req, res) => {
+    res.render('user/registerPage');
+  })
+  .post(async (req, res) => {
+    const { username, password, repeatPassword } = req.body;
 
-router.post('/register', async (req, res) => {
-  const { username, password, repeatPassword } = req.body;
+    await userManager.register({ username, password, repeatPassword });
+    res.redirect('/users/login');
+  });
 
-  await userManager.register({ username, password, repeatPassword });
-  res.redirect('/');
-});
+router
+  .route('/login')
+  .get((req, res) => {
+    res.render('user/loginPage');
+  })
+  .post(async (req, res) => {
+    const { username, password } = req.body;
+
+    const user = await userManager.login(username, password);
+    console.log(user);
+
+    res.cookie('username', user.username);
+    res.redirect('/');
+  });
 
 module.exports = router;
