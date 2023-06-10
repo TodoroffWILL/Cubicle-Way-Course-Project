521e3afe74f4473132f29f0fbb12919aa8a948f6
Doncho Todorov <108018258+TodoroffWILL@users.noreply.github.com>
2023-06-10 17:30:58 +0300

    validator added
     that i didn't understood..

---
+++ src/models/User.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+  username: String,
+  password: {
+    type: String,
+  },
+});
+
+userSchema.virtual('repeatPassword').set(function (value) {
+  if (value !== this.password) {
+    throw new mongoose.MongooseError('Password missmatch!');
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
